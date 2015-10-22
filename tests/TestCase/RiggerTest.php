8bbf89681077e78f6b556c8b620423c294d6df92
ligang <ligang@360.cn>
2015-10-22 18:33:53 +0800

    init

---
+++ tests/TestCase/RiggerTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Rigger\Test\TestCase;
+
+class RiggerTest extends \PHPUnit_Framework_TestCase
+{/*{{{*/
+    public function testConf()
+    {/*{{{*/
+    }/*}}}*/
+    public function testRestart()
+    {/*{{{*/
+    }/*}}}*/
+}/*}}}*/
---
+++ tests/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+$prjHome = dirname(dirname(__FILE__));
+
+require_once("$prjHome/vendor/autoload.php");
